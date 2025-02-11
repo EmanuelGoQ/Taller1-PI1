7248f55a77c6da4e52f1b683ddd621799113968b
Emanuel Gonzalez <egonzalezq@eafit.edu.co>
2025-02-10 22:51:09 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
 
 def home(request):
-    return render(request, 'home.html', {'name':'Emanuel Gonzalez'})
+    #return render(request, 'home.html', {'name':'Emanuel Gonzalez'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
+
 def about(request):
     return render(request, 'about.html')
